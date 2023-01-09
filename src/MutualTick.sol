8f6cc6c1130b21591f8a78d519084bb612ccc7b8
0xJilan <107534852+0xJilan@users.noreply.github.com>
2023-01-09 10:30:43 +0100

    add OZ and create remappings to facilitate lib imports in contracts

--- src/MutualTick.sol
+++ src/MutualTick.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract MutualTick {
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title MutualTick
+/// @author @0xJilan
+/// @notice implementation of Mutual Tick Option
+contract MutualTick is Ownable {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
